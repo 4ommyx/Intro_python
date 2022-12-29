7aa1731227639cfdb1815908112b6667de325c17
4ommyx <nattawutth011@gmail.com>
2022-12-29 22:51:44 +0700

    29-12-22

---
+++ Assignment/Assignment27.py
@@ -0,0 +1,11 @@
+# หาเลขคู่เลขคี่โดยใช้ function
+
+a = int(input("enter your num : "))
+
+def numm(n):
+    if n%2 != 0:
+        print("ood")
+    else:
+        print("even")
+
+numm(a)
---
+++ Ep/Main/Function/Ep02.py
@@ -0,0 +1,16 @@
+#การรับค่าเข้า function โดยระบบ parameter
+
+def name(a):
+    print("name : ",a)
+
+a = "music"
+name("aom")
+name(a)
+
+def name1(a,b):
+    print("name : ",a ,"  age : ",b)
+
+name1("aom",20)
+
+# Argument --> ค่าที่ส่งเข้าไปใน function (aom,20)
+# Parameter --> ค่าตัวแปรที่รับข้อมูลจาก Argument มาทำงาน (a,b) 
